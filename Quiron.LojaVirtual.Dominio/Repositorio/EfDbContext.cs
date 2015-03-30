16ba21e4e644cd5f65a9ffc4c0dd37fb3e38ce05
DanielGarcez <danielgarcez@outlook.com>
2015-03-30 18:51:36 -0300

    Inicio da Paginação

---
+++ Quiron.LojaVirtual.Dominio/Repositorio/EfDbContext.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Quiron.LojaVirtual.Dominio.Entidade;
+
+namespace Quiron.LojaVirtual.Dominio.Repositorio
+{
+    public class EfDbContext : DbContext
+    {
+
+        #region Tabelas
+        public DbSet<Produto> Produtos { get; set; }
+        #endregion
+
+
+        #region OnModelCreating
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+            modelBuilder.Entity<Produto>().ToTable("Produtos");
+        }
+        #endregion
+
+    }
+}
---
+++ Quiron.LojaVirtual.Web/Controllers/VitrineController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Quiron.LojaVirtual.Dominio.Repositorio;
+
+namespace Quiron.LojaVirtual.Web.Controllers
+{
+    public class VitrineController : Controller
+    {
+
+        private ProdutosRepositorio _produtosRepositorio;
+        private int _produtosPorPagina = 8;
+
+        public ActionResult ListaProdutos(int pagina = 1)
+        {
+            _produtosRepositorio = new ProdutosRepositorio();
+            var produtos = _produtosRepositorio.Produtos
+                                               .OrderBy(p => p.Nome)
+                                               .Skip((pagina-1)*_produtosPorPagina)
+                                               .Take(_produtosPorPagina);
+            return View(produtos);
+        }
+    }
+}
